3af02ff32db5a4fe37956888df83a3b51514b097
姜雨生 <826420016@qq.com>
2016-05-02 01:07:21 +0800

    no message

---
+++ WKWebViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebViewDemo
+//
+//  Created by 姜雨生 on 16/4/30.
+//  Copyright © 2016年 姜雨生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WKWebViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKWebViewDemo
+//
+//  Created by 姜雨生 on 16/4/30.
+//  Copyright © 2016年 姜雨生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
